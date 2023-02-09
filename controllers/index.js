eb3e548072359ee9c7dbdf8a827bfc1126d406ed
Ben L <veloswim@gmail.com>
2023-02-09 11:55:03 -0800

    added home.handlebars and route to render to page

--- controllers/index.js
+++ controllers/index.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
+const frontEndRoutes = require("./frontEndRoutes");
+router.use("/", frontEndRoutes);
+
 module.exports = router;
