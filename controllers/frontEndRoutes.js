a123cd910a6049583546c7fabc5e76aa91cbb967
Emma Waltho <116322906+ewaltho@users.noreply.github.com>
2023-02-09 23:43:33 -0500

    Update frontEndRoutes.js

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("signin");
+  res.render("home");
 });
 
 module.exports = router;
