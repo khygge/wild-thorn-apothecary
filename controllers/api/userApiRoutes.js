cb10657b0e1c88ee45c67c8d74f00df561890e88
Ben L <veloswim@gmail.com>
2023-02-10 00:04:25 -0800

    created route to add plant to user

--- controllers/api/userApiRoutes.js
+++ controllers/api/userApiRoutes.js
@@ -16,4 +16,29 @@ router.get("/:userid", async (req, res) => {
   res.json(findOneUser);
 });
 
+router.post("/", async (req, res) => {
+  const createNewuser = await User.create({
+    user_email: req.body.user_email,
+    username: req.body.username,
+    password: req.body.password,
+  });
+  res.send(createNewuser);
+});
+
+router.post("/:userid/:plantid", async (req, res) => {
+  const findOneUser = await User.findByPk(req.params.userid);
+  if (!findOneUser) {
+    res.status(404).json({ msg: "no such user" });
+  } else {
+    const checkValidPlant = await Plant.findByPk(req.params.plantid);
+    if (!checkValidPlant) {
+      res.status(404).json({ msg: "no such plant" });
+    } else {
+      const addPlantToUser = await findOneUser.addPlant(req.params.plantid);
+
+      res.json(addPlantToUser);
+    }
+  }
+});
+
 module.exports = router;
