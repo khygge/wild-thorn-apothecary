b7b0ff77299aa7a9924e1d7e74c99f3ed36af723
Ben L <veloswim@gmail.com>
2023-02-10 10:51:14 -0800

    added route to find all health benefits, fixed script source in layouts

--- controllers/api/healthApiRoutes.js
+++ controllers/api/healthApiRoutes.js
@@ -2,4 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { User, Plant, Health } = require("../../models");
 
+router.get("/", async (req, res) => {
+  const findHealth = await Health.findAll({ include: [Plant] });
+  res.json(findHealth);
+});
+
 module.exports = router;
