4f69c808ca134fcbee0931602f3ff93699f65ce5
Ben L <veloswim@gmail.com>
2023-02-10 10:55:59 -0800

    added try catch blocks in plant api routes

--- controllers/api/healthApiRoutes.js
+++ controllers/api/healthApiRoutes.js
@@ -3,8 +3,25 @@ const router = express.Router();
 const { User, Plant, Health } = require("../../models");
 
 router.get("/", async (req, res) => {
-  const findHealth = await Health.findAll({ include: [Plant] });
-  res.json(findHealth);
+  try {
+    const findHealth = await Health.findAll({ include: [Plant] });
+    res.json(findHealth);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occured", error: err });
+  }
+});
+
+router.get("/:healthid", async (req, res) => {
+  try {
+    const findOneHealth = await Health.findByPk(req.params.healthid, {
+      include: [Plant],
+    });
+    res.json(findOneHealth);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ msg: "an error occurred", error: err });
+  }
 });
 
 module.exports = router;
