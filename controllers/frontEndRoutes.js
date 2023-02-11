25c2193df44930a64642e281c6ad482a93d8c2ee
Ben L <veloswim@gmail.com>
2023-02-10 21:18:48 -0800

    added nodemailer function to frontendroutes, this is placeholder until formatted for plants

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const nodemailer = require("nodemailer");
 
 router.get("/", (req, res) => {
   res.render("home");
@@ -23,6 +24,38 @@ router.get("/userplants", (req, res) => {
 
 router.get("/ourplants", (req, res) => {
   res.render("plantslist");
+});
+
+let mailFunction = async () => {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "b.wildthorn@gmail.com", // generated ethereal user
+      pass: "ozvcfiuztyuzzrok",
+      tls: {
+        rejectUnAuthorized: true,
+      }, // generated ethereal password
+    },
+  });
+  // b.wildthorn@gmail.com
+  // BingusOurB0ngus!
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Wild Thorn No-reply" <b.wildthorn@yahoo.com>', // sender address
+    to: "veloswim@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+};
+
+router.get("/mail", async (req, res) => {
+  const mail = await mailFunction();
+  res.json(mail);
 });
 
 module.exports = router;
