b72aa4c0b3abeeb33a03b1176e53995d85a935a1
Ben L <veloswim@gmail.com>
2023-02-11 13:38:45 -0800

    added list to message generation

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const { User, Plant, Health } = require("../models");
 
 router.get("/", (req, res) => {
   res.render("home");
@@ -54,9 +55,38 @@ let mailFunction = async () => {
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 };
 
-router.get("/mail", async (req, res) => {
-  const mail = await mailFunction();
-  res.json(mail);
+router.get("/mail/:userid", async (req, res) => {
+  const findOneUser = await User.findByPk(req.params.userid, {
+    include: {
+      model: Plant,
+      include: [Health],
+    },
+  });
+  let allUserPlants = findOneUser.Plants;
+  for (let i = 0; i < allUserPlants.length; i++) {
+    let name = allUserPlants[i].toJSON().plant_name;
+    let type = allUserPlants[i].toJSON().type;
+    let health = allUserPlants[i].toJSON().Health;
+    let healthMsg = [];
+    for (let n = 0; n < health.length; n++) {
+      if (n + 1 == health.length) {
+        let message = ` ${health[n].benefits}.`;
+        healthMsg.push(message);
+        healthMsg.join("");
+      } else {
+        let message = ` ${health[n].benefits}`;
+        healthMsg.push(message);
+      }
+    }
+
+    const messageForOnePlant = `${
+      i + 1
+    }. ${name}, type: ${type}. The health benefits for this plant are:${healthMsg}`;
+    console.log(messageForOnePlant);
+  }
+  res.json(allUserPlants);
+  // const mail = await mailFunction();
+  // res.json(mail);
 });
 
 module.exports = router;
